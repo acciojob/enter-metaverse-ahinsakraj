c551920d3fcd2c13b156327fd0761d43b7665a60
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-23 00:59:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const btn=document.getElementById('enterBtn');
+btn.addEventListener('click',function enterMetavese() {
+	const element=document.getElementById("status");
+	
+	element.innerText="Entered Metaverse";
+	element.outerHTML=element.outerHTML.replace(/p/g,"h1");
+})
